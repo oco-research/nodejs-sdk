7a30584277acaa6957c4fd9bddd15fe7253d5d7d
OCO SDK Generator <devex-open-contribution-office@adobe.com>
2021-12-09 00:57:11 +0000

    1.0.8

---
+++ test/model/ApiResponse.spec.js
@@ -0,0 +1,69 @@
+/*
+* Garage Week 2021. All rights reserved.
+* Your custom license text goes here.
+*/
+
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD.
+    define(['expect.js', process.cwd()+'/src/index'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // CommonJS-like environments that support module.exports, like Node.
+    factory(require('expect.js'), require(process.cwd()+'/src/index'));
+  } else {
+    // Browser globals (root is window)
+    factory(root.expect, root.OcoResearchNodejsSdk);
+  }
+}(this, function(expect, OcoResearchNodejsSdk) {
+  'use strict';
+
+  var instance;
+
+  beforeEach(function() {
+    instance = new OcoResearchNodejsSdk.ApiResponse();
+  });
+
+  var getProperty = function(object, getter, property) {
+    // Use getter method if present; otherwise, get the property directly.
+    if (typeof object[getter] === 'function')
+      return object[getter]();
+    else
+      return object[property];
+  }
+
+  var setProperty = function(object, setter, property, value) {
+    // Use setter method if present; otherwise, set the property directly.
+    if (typeof object[setter] === 'function')
+      object[setter](value);
+    else
+      object[property] = value;
+  }
+
+  describe('ApiResponse', function() {
+    it('should create an instance of ApiResponse', function() {
+      // uncomment below and update the code to test ApiResponse
+      //var instance = new OcoResearchNodejsSdk.ApiResponse();
+      //expect(instance).to.be.a(OcoResearchNodejsSdk.ApiResponse);
+    });
+
+    it('should have the property code (base name: "code")', function() {
+      // uncomment below and update the code to test the property code
+      //var instance = new OcoResearchNodejsSdk.ApiResponse();
+      //expect(instance).to.be();
+    });
+
+    it('should have the property type (base name: "type")', function() {
+      // uncomment below and update the code to test the property type
+      //var instance = new OcoResearchNodejsSdk.ApiResponse();
+      //expect(instance).to.be();
+    });
+
+    it('should have the property message (base name: "message")', function() {
+      // uncomment below and update the code to test the property message
+      //var instance = new OcoResearchNodejsSdk.ApiResponse();
+      //expect(instance).to.be();
+    });
+
+  });
+
+}));
